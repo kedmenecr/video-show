c11b46c74888fc35bcdbd0e40c4b8a78ecc59d14
Roberto Kedmenec <rexerd96@gmail.com>
2018-02-04 21:16:43 +0100

    Laratrust

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'role_structure' => [
+        'superadministrator' => [
+            'users' => 'c,r,u,d',
+            'acl' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
